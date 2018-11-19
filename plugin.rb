8385b1a4ff6644fb42ceb9792a9da30fbd61177d
Madeline OLeary <madeline.w.oleary@gmail.com>
2018-11-19 18:02:34 +0100

    independent serializer to reduce conflicts with core

--- plugin.rb
+++ plugin.rb
@@ -4,14 +4,17 @@
 # authors: Madeline O'Leary
 
 after_initialize do
-  require_dependency "app/models/user"
+  load File.expand_path('../serializers/icij_group_user_serializer.rb', __FILE__)
+
+  require_dependency "user"
   class ::User
     def self.all_group_users(current_user)
       icij_group_ids = Group.icij_groups.pluck(:id)
       current_user_groups = current_user.group_users.where(group_id: icij_group_ids).pluck(:group_id)
       all_users_part_of_current_users_groups = GroupUser.where(group_id: current_user_groups).pluck(:user_id).uniq
-      users_to_display = User.where(id: all_users_part_of_current_users_groups)
-      users_to_display
+      users = User.where(id: all_users_part_of_current_users_groups)
+
+      users
     end
 
     def added_at
@@ -19,12 +22,12 @@ after_initialize do
     end
   end
 
-  require_dependency "app/controllers/application_controller"
+  require_dependency "application_controller"
   UsersController.class_eval do
     def all_users_in_groups
       users_to_display = User.all_group_users(current_user)
       render json: {
-        members: serialize_data(users_to_display, GroupUserSerializer)
+        members: serialize_data(users_to_display, IcijGroupUserSerializer)
       }
     end
   end
---
+++ serializers/icij_group_user_serializer.rb
@@ -0,0 +1,27 @@
+class IcijGroupUserSerializer < ApplicationSerializer
+  attributes :title, :last_posted_at, :last_seen_at, :added_at, :id, :username, :name, :avatar_template
+
+  def name
+    Hash === user ? user[:name] : user.try(:name)
+  end
+
+  def include_name?
+    SiteSetting.enable_names?
+  end
+
+  def avatar_template
+    if Hash === object
+      User.avatar_template(user[:username], user[:uploaded_avatar_id])
+    else
+      user&.avatar_template
+    end
+  end
+
+  def user
+    object[:user] || object
+  end
+
+  def include_added_at
+    object.respond_to? :added_at
+  end
+end
